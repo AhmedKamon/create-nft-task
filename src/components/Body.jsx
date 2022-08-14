42635167ccb65115c194a16c1516641b48051ff3
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-08-14 03:04:07 -0700

    project

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,28 @@
-import Progress from "../constent/Progress"
-
+import Progress from "../constent/Progress";
+import { details } from "../utils/static";
 
 function Body() {
   return (
-   <Progress/>
-  )
+    <div className="lg:px-28 px-11 ">
+      <Progress />
+      <div className="flex  flex-col md:flex-row md:justify-between items-center cursor-pointer my-5  ">
+        {details.map((item) => (
+          <div
+            className={`flex items-center rounded-md gap-3 py-3 lg:px-9 md:px-5 px-3  text-sm mb-5  ${
+              item.id === 3
+                ? `bg-[#0A8A97] shadow-lg shadow-[#0A8A97]/50 `
+                : `bg-[#58DBBA]`
+            } `}
+            key={item.id}
+          >
+            <img className="w-7 h-7 " src={item.icon} alt="" />
+
+            <h1 className="text-white font-medium">{item.name}</h1>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Body
+export default Body;
--- src/constent/Progress.jsx
+++ src/constent/Progress.jsx
@@ -3,7 +3,7 @@ import ProgressBar from "@ramonak/react-progress-bar";
 
 function Progress() {
   return (
-    <div className='px-28 font-medium text-4xl'>
+    <div className='font-medium text-4xl'>
         <h1 className='my-11'>Create Project NFT</h1>
         <ProgressBar completed={70} bgColor='#22D6AC' borderRadius='5px' height='40px' baseBgColor='#ECECEC' labelAlignment='center' />
     </div>
