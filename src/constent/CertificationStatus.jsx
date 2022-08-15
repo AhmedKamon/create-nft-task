2e90d728808c28b243c35e781d400cbcbfccc4ec
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-08-14 23:04:28 -0700

    radio btns

---
+++ src/constent/CertificationStatus.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+function CertificationStatus({handelChange,formData}) {
+  return (
+    <div>
+      <div className="grid grid-cols-2 gap-4 ">
+        <h1 className="text-[#0A8A97] font-medium ">Certification Status</h1>
+        <div className="flex  justify-between items-end ">
+          <div className="flex items-center">
+            <input
+              id="yes"
+              type="radio"
+              value="yes"
+              name="certification_status"
+              className="w-5 h-5 accent-[#2ADBA7] bg-transparent border-gray-300 "
+              onChange={handelChange}
+              checked={formData.certification_status === "yes"}
+            />
+            <label  htmlFor="yes" className="ml-2  font-medium text-[#5C5C5C] ">
+              Yes
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              id="pending"
+              type="radio"
+              name="certification_status"
+              className="w-5 h-5 accent-[#2ADBA7] bg-transparent border-gray-300 "
+              value={"pending"}
+              onChange={handelChange}
+              checked={formData.certification_status === "pending"}
+            />
+            <label htmlFor="pending" className="ml-2 font-medium text-[#5C5C5C] ">
+              Pending
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              id="no"
+              type="radio"
+              value={"no"}
+              name="certification_status"
+              className="w-5 h-5 accent-[#2ADBA7] bg-transparent border-gray-300 "
+              onChange={handelChange}
+              checked={formData.certification_status === "no"}
+            />
+            <label htmlFor="no" className="ml-2 font-medium text-[#5C5C5C] ">
+              No
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="border-b-[1px] border-[#D9D9D9] mt-3"></div>
+    </div>
+  );
+}
+
+export default CertificationStatus;
--- src/constent/Table.jsx
+++ src/constent/Table.jsx
@@ -1,51 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import CertificationStatus from "./CertificationStatus";
 
 function Table() {
+  const [formData, setFormData] = useState({
+    certification_status:'pending',
+  });
+  const handelChange = (e) => {
+    console.log(e);
+    const value =
+      e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    const name = e.target.name;
+    console.log(name)
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const handelSubmit =  (e) => {
+    e.preventDefault()
+    console.log(formData,'e')
+  };
   return (
     <div className="p-16 ">
-      <div className="grid grid-cols-2 gap-4 ">
-        <h1 className="text-[#0A8A97] font-medium ">Certification Status</h1>
-        <div className="flex  justify-between items-end ">
-          <div class="flex items-center">
-            <input
-              id="yes"
-              type="radio"
-              value={"yes"}
-              name="choose"
-              className="w-5 h-5 accent-[#2ADBA7] bg-transparent border-gray-300 "
-            />
-            <label for="yes" class="ml-2  font-medium text-[#5C5C5C] ">
-              Yes
-            </label>
-          </div>
-          <div class="flex items-center">
-            <input
-              id="pending"
-              type="radio"
-              value={"pending"}
-              name="choose"
-              className="w-5 h-5 accent-[#2ADBA7] bg-transparent border-gray-300 "
-            />
-            <label for="no" class="ml-2 font-medium text-[#5C5C5C] ">
-              Pending
-            </label>
-          </div>
-          <div class="flex items-center">
-            <input
-              id="no"
-              type="radio"
-              value={"no"}
-              name="choose"
-              className="w-5 h-5 accent-[#2ADBA7] bg-transparent border-gray-300 "
-            />
-            <label for="no" class="ml-2 font-medium text-[#5C5C5C] ">
-              No
-            </label>
-          </div>
-        </div>
-      </div>
-      <div class="border-b-[1px] border-[#D9D9D9] mt-3">
-</div>
+      <form action="" onSubmit={handelSubmit} >
+        <CertificationStatus handelChange={handelChange} formData={formData} />
+        <input type="submit" name="" value='Create Project NFT' id="" className="bg-gray-500 px-4 py-2 " />
+      </form>
     </div>
   );
 }
